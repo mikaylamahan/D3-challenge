ecf375b15149f748347d1c433e431573037b6193
mikaylamahan <mroller21@gmail.com>
2020-01-11 16:46:38 -0700

    save

--- D3_challenge/DU-DEN-DATA-PT-08-2019-U-C-master-16-D3-Homework-Instructions/16-D3/Homework/Instructions/StarterCode/assets/js/app.js
+++ D3_challenge/DU-DEN-DATA-PT-08-2019-U-C-master-16-D3-Homework-Instructions/16-D3/Homework/Instructions/StarterCode/assets/js/app.js
@@ -1,1 +1,39 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Attach to the scatter section of the body of the index.html. Set the dimensions 
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+d3.csv("/assets/data/data.csv").then(function(totaldata) {
+
+    // Print the totaldata to see if it loads correctly
+    console.log(totaldata);
+    
+    // Convert value to number
+    healthpovData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    }
+
+})
+  
