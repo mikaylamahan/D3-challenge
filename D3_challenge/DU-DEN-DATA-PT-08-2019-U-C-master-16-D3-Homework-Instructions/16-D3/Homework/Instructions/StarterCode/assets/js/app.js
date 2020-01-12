c04b1240a0f4b6e1fef731d1a9adea9cd3a5923e
mikaylamahan <mroller21@gmail.com>
2020-01-11 17:26:01 -0700

    update

--- D3_challenge/DU-DEN-DATA-PT-08-2019-U-C-master-16-D3-Homework-Instructions/16-D3/Homework/Instructions/StarterCode/assets/js/app.js
+++ D3_challenge/DU-DEN-DATA-PT-08-2019-U-C-master-16-D3-Homework-Instructions/16-D3/Homework/Instructions/StarterCode/assets/js/app.js
@@ -28,12 +28,81 @@ d3.csv("/assets/data/data.csv").then(function(totaldata) {
 
     // Print the totaldata to see if it loads correctly
     console.log(totaldata);
-    
+
     // Convert value to number
-    healthpovData.forEach(function(data) {
+    totaldata.forEach(function(data) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
-    }
+    });
 
-})
-  
+    //create a y scale
+
+    var xLinearScale = d3.scaleLinear()
+    .domain([0, d3.min(totaldata, d=>d.poverty),
+        d3.max(totaldata, d => d.poverty)])
+    .range([chartHeight, 0]);
+
+
+    //create an x scale
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(totaldata, d => d.healthcare)])
+    .range([chartHeight, 0])
+
+    //set axes
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    
+    //now call the bottom and left axis
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .style("font-size", "16px")
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .style("font-size", "16px")
+        .call(leftAxis);
+    
+    //create blue, opaque circles for the points on the graph
+    chartGroup.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 12)
+        .attr("fill", "blue")
+        .attr("opacity", ".6");
+
+    //include state abbrvs inside the small circles, per instructions
+    chartGroup.selectAll("text.text-circles")
+    .data(data)
+    .enter()
+    .append("text")
+    .classed("text-circles",true)
+    .text(d => d.abbr)
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("dy",5)
+    .attr("text-anchor","middle")
+    .attr("font-size","14px")
+    .attr("fill", "white");
+
+    //create/append y axis
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 30 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .text("Lacks Healthcare (%)");
+    //create/append x axis
+    chartGroup.append("text")
+        .attr("y", height + margin.bottom/2 - 10)
+        .attr("x", width / 2)
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .text("Poverty Rate (%)");
+    
+});
+
